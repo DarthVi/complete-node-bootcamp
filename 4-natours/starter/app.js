71fc096386fd95e75fc3ebe7c7a2d3e0d35ccb8e
DarthVi <>
2022-07-21 10:02:41 +0200

    Handling POST requets

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,18 +1,11 @@
 const fs = require('fs');
+const fsPromise = require('fs/promises');
 const express = require('express');
 
 const app = express();
 
-/*app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello from the server side!',
-    app: 'Natours',
-  });
-});
-
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});*/
+//add a middleware, which is a function that can modify the incoming request data
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -26,6 +19,33 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', async (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  //lets us create a new object by merging existing objects
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  try {
+    await fsPromise.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours)
+    );
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      data: {
+        message: err.message,
+      },
+    });
+  }
 });
 
 const port = 3000;
