b77e51d4576f25169e7fc5bffd3f064acdc820e6
DarthVi <>
2022-08-03 15:02:13 +0200

    Refactoring code before using Mongoose and MongoDB

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,20 +1,4 @@
-const fs = require('fs');
-const fsPromise = require('fs/promises');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price)
@@ -29,50 +13,33 @@ exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     requestedAt: req.requestTime,
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // status: 'success',
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1; //trick to convert string to number
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = async (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  //lets us create a new object by merging existing objects
-  // eslint-disable-next-line prefer-object-spread
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  try {
-    await fsPromise.writeFile(
-      `${__dirname}/../dev-data/data/tours-simple.json`,
-      JSON.stringify(tours)
-    );
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
 };
 
 exports.updateTour = (req, res) => {
