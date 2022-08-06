a4e8425c0fdbe30f40d953ea2782d385fbed590d
DarthVi <>
2022-08-06 09:45:43 +0200

    implemented limiting fields in get all tours query API

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -3,7 +3,7 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
-    //1)Filtering
+    //1A)Filtering
     //hard copy of the req.query using an ES6 query using destructuring
     //and creating a new object
     const queryObj = { ...req.query };
@@ -11,11 +11,27 @@ exports.getAllTours = async (req, res) => {
     //and other stuff
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    //2) Advanced filtering
+    //1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2)Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //3)Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
 
     //EXECUTE QUERY
     const tours = await query;
