97081ea2522fabe8d0a76bb2606e3b5aea89e94b
DarthVi <>
2022-09-13 18:20:23 +0200

    Handled Mongoose validation errors

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -27,7 +27,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a difficulty'],
       enum: {
         values: ['easy', 'medium', 'difficult'],
-        message: 'Difficult is either: easy, medium, difficult',
+        message: 'Difficulty is either: easy, medium, difficult',
       },
     },
     ratingsAverage: {
